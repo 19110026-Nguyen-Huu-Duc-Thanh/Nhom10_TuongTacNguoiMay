7ebc25f519a281af691a4b1f2e9713dd34818f3a
Quocnea1 <101201594+Quocnea1@users.noreply.github.com>
2023-05-06 13:53:05 +0700

    Quoc-frontEnd

--- front-end/src/app/store.js
+++ front-end/src/app/store.js
@@ -1,21 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice";
-// import addressReducer from "../slices/addressSlice";
+import addressReducer from "../slices/addressSlice";
 import productReducer from "../slices/productSlice";
 import categoryReducer from "../slices/categorySlice";
 import cartReducer from "../slices/cartSlice";
 // import userReducer from "../slices/userSlice";
-// import orderReducer from "../slices/orderSlice";
+import orderReducer from "../slices/orderSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
-    // address: addressReducer,
+    address: addressReducer,
     product: productReducer,
     category: categoryReducer,
     // user: userReducer,
-    // order: orderReducer,
+    order: orderReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
